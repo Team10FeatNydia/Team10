6a0a20a75a2d0d377c266cea2d0ae683224e2c2f
Sean Han Li Ze <seanhlz@hotmail.com>
2017-09-15 00:41:54 +0800

    Finish creating Main Menu Placeholder, have not implemented Phone touch function -Added Level Select -Added Audio -Added Settings for Audio -Added Level 1
    The way to add level is to name its thumbnail picture the same name as the game scene itself, theres a prefab that automatically puts new level into the level select screen
    
    Anything in this build is not final and is subjected to change

---
+++ Assets/Script/Level1Script.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Level1Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+        SoundManagerScript.Instance.PlayBGM(AudioClipID.BGM_LEVEL_1);
+    }
+}
