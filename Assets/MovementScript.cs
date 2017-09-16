b47f9a2eefa82da8fa136115c56aad9e5ff2a97d
Kebin of Wonders <nubfight@gmail.com>
2017-09-16 18:49:34 +0800

    Movement + Touch controls done.

---
+++ Assets/MovementScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementScript : MonoBehaviour {
+
+    public NavMeshAgent playerAgent;
+    private RaycastHit interactionInfo;
+    private GameObject interactedObject;
+
+    private LayerMask floorLayer;
+
+    // Use this for initialization
+    void Start () {
+        playerAgent = GetComponent<NavMeshAgent>();
+        floorLayer = LayerMask.GetMask("FloorLayer");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Movement();
+	}
+
+    void Movement()
+    {
+
+        if (Input.touchCount == 1)
+        {
+            Ray ray;
+
+            foreach (Touch touch in Input.touches)
+            {
+                if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Stationary)
+                {
+                    ray = Camera.main.ScreenPointToRay(touch.position);
+
+                    if (Physics.Raycast(ray, out interactionInfo, Mathf.Infinity, floorLayer))
+                    {
+                        interactedObject = interactionInfo.transform.gameObject;
+                        playerAgent.destination = interactionInfo.point;
+                    }
+                }
+                
+            }
+        }
+    }
+}
